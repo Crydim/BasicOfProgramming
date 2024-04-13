34f3fc34e4f8ee842b324e354aea9ec8d36f3bae
Crydim <potrehaevdima@gmail.com>
2024-04-14 00:32:05 +0300

    findNonSpaceReverse function

--- libs/data_structures/string/string_.c
+++ libs/data_structures/string/string_.c
@@ -26,3 +26,10 @@ char* findSpace(char *begin){
     }
     return begin;
 }
+
+char* findNonSpaceReverse(char *rbegin, const char *rend){
+    while(rbegin != rend && isspace(*rbegin)){
+        rbegin--;
+    }
+    return rbegin;
+}
